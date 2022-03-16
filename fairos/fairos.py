9b40541490706b48dc8da2b1b3e7b64e70e99b9b
shepherliu <84829620+shepherliu@users.noreply.github.com>
2022-03-16 12:00:42 +0800

    fix

--- fairos/fairos.py
+++ fairos/fairos.py
@@ -5,7 +5,11 @@ import sys
 import json
 import requests
 
-import modules
+from .modules import db
+from .modules import fs as filesystem
+from .modules import kv
+from .modules import pod
+from .modules import user
 
 class Fairos(object):
 	"""docstring for Fairos"""
@@ -36,12 +40,12 @@ class Fairos(object):
 	#https://docs.modules.fairdatasociety.org/api/#tag/User/paths/~1user~1signup/post
 	def signup_user(user_name:str, password:str, mnemonic = ''):
 
-		return modules.signup_user(user_name, password, mnemonic = mnemonic, host = self.__host)
+		return user.signup_user(user_name, password, mnemonic = mnemonic, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/User/paths/~1user~1login/post
 	def login_user(user_name:str, password:str):
 
-		res = modules.login_user(user_name, password, host = self.__host)
+		res = user.login_user(user_name, password, host = self.__host)
 
 		if res['message'] == 'success':
 			self.__user_name = user_name
@@ -53,7 +57,7 @@ class Fairos(object):
 	#https://docs.modules.fairdatasociety.org/api/#tag/User/paths/~1user~1import/post
 	def import_user(user_name:str, password:str, address:str):
 
-		res = modules.import_user(user_name, password, address, host = self.__host)
+		res = user.import_user(user_name, password, address, host = self.__host)
 
 		if res['message'] == 'success':
 			self.__user_name = user_name
@@ -65,17 +69,17 @@ class Fairos(object):
 	#https://docs.modules.fairdatasociety.org/api/#tag/User/paths/~1user~1present/get
 	def user_present(user_name:str):
 
-		return modules.user_present(user_name, host = self.__host)
+		return user.user_present(user_name, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/User/paths/~1user~1isloggedin/get
 	def is_logged_in(user_name:str):
 		
-		return modules.is_logged_in(user_name, host = self.__host)
+		return user.is_logged_in(user_name, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/User/paths/~1user~1logout/post
 	def user_logout():
 
-		res = modules.user_logout(cookies = self.__cookies, host = self.__host)	
+		res = user.user_logout(cookies = self.__cookies, host = self.__host)	
 
 		self.__cookies = None
 		self.__user_name = ''
@@ -86,12 +90,12 @@ class Fairos(object):
 	#https://docs.modules.fairdatasociety.org/api/#tag/User/paths/~1user~1export/post
 	def export_user():
 
-		return modules.export_user(cookies = self.__cookies, host = self.__host)
+		return user.export_user(cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/User/paths/~1user~1delete/delete
 	def delete_user(password:str):
 
-		res = modules.delete_user(password, cookies = self.__cookies, host = self.__host)
+		res = user.delete_user(password, cookies = self.__cookies, host = self.__host)
 
 		self.__cookies = None
 		self.__user_name = ''
@@ -102,24 +106,24 @@ class Fairos(object):
 	#https://docs.modules.fairdatasociety.org/api/#tag/User/paths/~1user~1stat/get
 	def user_stat():
 
-		return modules.user_stat(cookies = self.__cookies, host = self.__host)
+		return user.user_stat(cookies = self.__cookies, host = self.__host)
 
 	"""pod functions """
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1receiveinfo/get
 	def pod_receiveinfo(reference:str):
 
-		return modules.pod_receiveinfo(reference, cookies = self.__cookies, host = self.__host)
+		return pod.pod_receiveinfo(reference, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1receive/get
 	def pod_receive(reference:str):
 
-		return modules.pod_receive(reference, cookies = self.__cookies, host = self.__host)
+		return pod.pod_receive(reference, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1new/post
 	def new_pod(pod_name:str):
 
-		res = modules.new_pod(pod_name, self.__password, cookies = self.__cookies, host = self.__host)
+		res = pod.new_pod(pod_name, self.__password, cookies = self.__cookies, host = self.__host)
 
 		if res['message'] == 'success':
 			self.__cookies = res['cookies']
@@ -129,7 +133,7 @@ class Fairos(object):
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1open/post
 	def open_pod(pod_name:str):
 
-		res = modules.open_pod(pod_name, self.__password, cookies = self.__cookies, host = self.__host)
+		res = pod.open_pod(pod_name, self.__password, cookies = self.__cookies, host = self.__host)
 
 		if res['message'] == 'success':
 			self.__cookies = res['cookies']
@@ -139,99 +143,99 @@ class Fairos(object):
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1close/post
 	def close_pod(pod_name:str):
 
-		return modules.close_pod(pod_name, cookies = self.__cookies, host = self.__host)
+		return pod.close_pod(pod_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1sync/post
 	def sync_pod(pod_name:str):
 
-		return modules.sync_pod(pod_name, cookies = self.__cookies, host = self.__host)
+		return pdo.sync_pod(pod_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1share/post
 	def share_pod(pod_name:str):
 
-		return modules.share_pod(pod_name, self.__password, cookies = self.__cookies, host = self.__host)
+		return pod.share_pod(pod_name, self.__password, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1delete/delete
 	def delete_pod(pod_name:str):
 
-		return modules.delete_pod(pod_name, self.__password, cookies = self.__cookies, host = self.__host)
+		return pod.delete_pod(pod_name, self.__password, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1ls/get
 	def list_pod():
 
-		return modules.list_pod(cookies = self.__cookies, host = self.__host)
+		return pod.list_pod(cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1stat/get
 	def stat_pod(pod_name:str):
 
-		return modules.stat_pod(pod_name, cookies = self.__cookies, host = self.__host)
+		return pod.stat_pod(pod_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Pod/paths/~1pod~1present/get
 	def pod_present(pod_name:str):
 
-		return modules.pod_present(pod_name, cookies = self.__cookies, host = self.__host)
+		return pod.pod_present(pod_name, cookies = self.__cookies, host = self.__host)
 
 	"""filesystem functions"""
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1dir~1mkdir/post
 	def make_dir(pod_name:str, dir_path:str):
 
-		return modules.make_dir(pod_name, dir_path, cookies = self.__cookies, host = self.__host)
+		return filesystem.make_dir(pod_name, dir_path, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1dir~1rmdir/delete
 	def remove_dir(pod_name:str, dir_path:str):
 
-		return modules.remove_dir(pod_name, dir_path, cookies = self.__cookies, host = self.__host)
+		return filesystem.remove_dir(pod_name, dir_path, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1dir~1ls/get
 	def list_dir(pod_name:str, dir_path:str):
 
-		return modules.list_dir(pod_name, dir_path, cookies = self.__cookies, host = self.__host)
+		return filesystem.list_dir(pod_name, dir_path, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1dir~1stat/get
 	def stat_dir(pod_name:str, dir_path:str):
 
-		return modules.stat_dir(pod_name, dir_path, cookies = self.__cookies, host = self.__host)
+		return filesystem.stat_dir(pod_name, dir_path, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1dir~1present/get
 	def dir_present(pod_name:str, dir_path:str):
 
-		return modules.dir_present(pod_name, dir_path, cookies = self.__cookies, host =self.__host)
+		return filesystem.dir_present(pod_name, dir_path, cookies = self.__cookies, host =self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1file~1upload/post
 	def upload_file(pod_name:str, pod_dir:str, filename:str, block_size = '512'):
 
-		return modules.upload_file(pod_name, pod_dir, filename, block_size = block_size, cookies = self.__cookies, host = self.__host)
+		return filesystem.upload_file(pod_name, pod_dir, filename, block_size = block_size, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1file~1download/get
 	def download_file(pod_name:str, file_path:str, request_type = 'post'):
 
-		return modules.download_file(pod_name, file_path, request_type = request_type, cookies = self.__cookies, host = self.__host)
+		return filesystem.download_file(pod_name, file_path, request_type = request_type, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1file~1share/post
 	def share_file(pod_name:str, pod_path_file:str, dest_user:str):
 
-		return modules.share_file(pod_name, pod_path_file, dest_user, cookies = self.__cookies, host = self.__host)
+		return filesystem.share_file(pod_name, pod_path_file, dest_user, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1file~1receive/get
 	def receive_file(pod_name:str, sharing_ref:str, dir_path:str):
 
-		return modules.receive_file(pod_name, sharing_ref, dir_path, cookies = self.__cookies, host = self.__host)
+		return filesystem.receive_file(pod_name, sharing_ref, dir_path, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1file~1receiveinfo/get
 	def receive_file_info(pod_name:str, sharing_ref:str):
 
-		return modules.receive_file_info(pod_name, sharing_ref, cookies = self.__cookies, host = self.__host)
+		return filesystem.receive_file_info(pod_name, sharing_ref, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1file~1delete/delete
 	def delete_info(pod_name:str, file_path:str):
 
-		return modules.delete_info(pod_name, file_path, cookies = self.__cookies, host = self.__host)
+		return filesystem.delete_info(pod_name, file_path, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/File-System/paths/~1file~1stat/get
 	def stat_info(pod_name:str, file_path:str):
 
-		return modules.stat_info(pod_name, file_path, cookies = self.__cookies, host = self.__host)
+		return filesystem.stat_info(pod_name, file_path, cookies = self.__cookies, host = self.__host)
 
 	"""kv functions """
 
@@ -239,120 +243,120 @@ class Fairos(object):
 	#indexType = 'string' or 'number'
 	def create_new_table(pod_name:str, table_name:str, indexType = 'string'):
 
-		return modules.create_new_table(pod_name, table_name, indexType, cookies = self.__cookies, host = self.__host)
+		return kv.create_new_table(pod_name, table_name, indexType, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1ls/get
 	def list_tables(pod_name:str):
 
-		return modules.list_tables(pod_name, cookies = self.__cookies, host = self.__host)
+		return kv.list_tables(pod_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1open/post
 	def open_table(pod_name:str, table_name:str):
 
-		return modules.open_table(pod_name, table_name, cookies = self.__cookies, host = self.__host)
+		return kv.open_table(pod_name, table_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1count/post
 	def count_table(pod_name:str, table_name:str):
 
-		return modules.count_table(pod_name, table_name, cookies = self.__cookies, host = self.__host)
+		return kv.count_table(pod_name, table_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1delete/delete
 	def delete_table(pod_name:str, table_name:str):
 
-		return modules.delete_table(pod_name, table_name, cookies = self.__cookies, host = self.__host)
+		return kv.delete_table(pod_name, table_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1entry~1put/post
 	def put_key_value(pod_name:str, table_name:str, key:str, value:str):
 
-		return modules.put_key_value(pod_name, table_name, key, value, cookies = self.__cookies, host = self.__host)
+		return kv.put_key_value(pod_name, table_name, key, value, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1entry~1get/get
 	#format = '' or 'string' or 'byte-string'
 	def get_value(pod_name:str, table_name:str, key:str, format = ''):
 
-		return modules.get_value(pod_name, table_name, key, format = format, cookies = self.__cookies, host = self.__host)
+		return kv.get_value(pod_name, table_name, key, format = format, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1entry~1del/delete
 	def delete_value(pod_name:str, table_name:str, key:str):
 
-		return modules.delete_value(pod_name, table_name, key, cookies = self.__cookies, host = self.__host)
+		return kv.delete_value(pod_name, table_name, key, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1seek/post
 	def seek_key(pod_name:str, table_name:str, start:str, end:str, limit:int):
 
-		return modules.seek_key(pod_name, table_name, start, end, limit, cookies = self.__cookies, host = self.__host)
+		return kv.seek_key(pod_name, table_name, start, end, limit, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1seek~1next/get
 	def get_next(pod_name:str, table_name:str):
 
-		return modules.get_next(pod_name, table_name, cookies = self.__cookies, host = self.__host)
+		return kv.get_next(pod_name, table_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1loadcsv/post
 	def load_csv(pod_name:str, table_name:str, memory:str):
 
-		return modules.load_csv(pod_name, table_name, memory, cookies = self.__cookies, host = self.__host)
+		return kv.load_csv(pod_name, table_name, memory, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Key-Value-Store/paths/~1kv~1present/get
 	def key_present(pod_name:str, table_name:str, key:str):
 
-		return modules.key_present(pod_name, table_name, key, cookies = self.__cookies, host = self.__host)
+		return kv.key_present(pod_name, table_name, key, cookies = self.__cookies, host = self.__host)
 
 	"""document functions"""
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1new/post
 	def create_documentDB(pod_name:str, table_name:str, si:str, mutable:bool):
 
-		return modules.create_documentDB(pod_name, table_name, si, mutable, cookies = self.__cookies, host = self.__host)
+		return db.create_documentDB(pod_name, table_name, si, mutable, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1ls/get
 	def list_documentDBs(pod_name:str):
 
-		return modules.list_documentDBs(pod_name, cookies = self.__cookies, host = self.__host)
+		return db.list_documentDBs(pod_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1open/post
 	def open_documentDB(pod_name:str, table_name:str):
 
-		return modules.open_documentDB(pod_name, table_name, cookies = self.__cookies, host = self.__host)
+		return db.open_documentDB(pod_name, table_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1count/post
 	def count_documents(pod_name:str, table_name:str):
 
-		return modules.count_documents(pod_name, table_name, cookies = self.__cookies, host = self.__host)
+		return db.count_documents(pod_name, table_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1delete/delete
 	def delete_documentDB(pod_name:str, table_name:str):
 
-		return modules.delete_documentDB(pod_name, table_name, cookies = self.__cookies, host = self.__host)
+		return db.delete_documentDB(pod_name, table_name, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1find/get
 	def find_documents(pod_name:str, table_name:str, expr:str, limit:int):
 
-		return modules.find_documents(pod_name, table_name, expr, limit, cookies = self.__cookies, host = self.__host)
+		return db.find_documents(pod_name, table_name, expr, limit, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1loadjson/post
 	def load_json(pod_name:str, table_name:str, filename:str):
 
-		return modules.load_json(pod_name, table_name, filename, cookies = self.__cookies, host = self.__host)
+		return db.load_json(pod_name, table_name, filename, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1indexjson/post
 	def index_json(pod_name:str, table_name:str, file:str):
 
-		return modules.index_json(pod_name, table_name, file, cookies = self.__cookies, host = self.__host)
+		return db.index_json(pod_name, table_name, file, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1entry~1put/post
 	def put_document(pod_name:str, table_name:str, doc:str):
 
-		return modules.put_document(pod_name, table_name, doc, cookies = self.__cookies, host = self.__host)
+		return db.put_document(pod_name, table_name, doc, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1entry~1get/get
 	def get_document(pod_name:str, table_name:str, id:str):
 
-		return modules.get_document(pod_name, table_name, id, cookies = self.__cookies, host = self.__host)
+		return db.get_document(pod_name, table_name, id, cookies = self.__cookies, host = self.__host)
 
 	#https://docs.modules.fairdatasociety.org/api/#tag/Document-DB/paths/~1doc~1entry~1del/delete
 	def delete_document(pod_name:str, table_name:str, id:str):
 
-		return modules.delete_document(pod_name, table_name, id, cookies = self.__cookies, host = self.__host)
+		return db.delete_document(pod_name, table_name, id, cookies = self.__cookies, host = self.__host)
 
 if __name__ == '__main__':
 
